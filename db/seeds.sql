c8d36792b481c779067cadd34fb9b5ecae157606
jon <jcrossguitar@gmail.com>
2019-09-29 07:05:21 -0700

    I reworked the seeds to take up less room while also updating the burgers table.

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,21 +2,12 @@
 USE burgers_db;
 
 -- Insert a set of records.
-INSERT INTO tasks (burger_name) VALUES ('Breakfast Burger');
-INSERT INTO tasks (devoured) VALUES ('false');
-INSERT INTO tasks (burger_name) VALUES ('BLTO Burger');
-INSERT INTO tasks (devoured) VALUES ('true');
-INSERT INTO tasks (burger_name) VALUES ('BLTO Cheese Burger');
-INSERT INTO tasks (devoured) VALUES ('false');
-INSERT INTO tasks (burger_name) VALUES ('Veggie Burger');
-INSERT INTO tasks (devoured) VALUES ('false');
-INSERT INTO tasks (burger_name) VALUES ('Assorted Mushroom Burger');
-INSERT INTO tasks (devoured) VALUES ('true');
-INSERT INTO tasks (burger_name) VALUES ('Black Bean Burger');
-INSERT INTO tasks (devoured) VALUES ('false');
-INSERT INTO tasks (burger_name) VALUES ('Hawaiian Burger');
-INSERT INTO tasks (devoured) VALUES ('true');
-INSERT INTO tasks (burger_name) VALUES ('Double Trouble Burger');
-INSERT INTO tasks (devoured) VALUES ('true');
-INSERT INTO tasks (burger_name) VALUES ('Pink Paste Burger');
-INSERT INTO tasks (devoured) VALUES ('true');
+INSERT INTO tasks (burger_name, devoured, toppoings, price) VALUES ('Breakfast Burger', false, 2, 5.99);
+INSERT INTO tasks (burger_name, devoured, toppoings, price) VALUES ('BLTO Burger', false, 2, 5.99);
+INSERT INTO tasks (burger_name, devoured, toppoings, price) VALUES ('BLTO Cheese Burger', false, 2, 5.99);
+INSERT INTO tasks (burger_name, devoured, toppoings, price) VALUES ('Veggie Burger', false, 2, 5.99);
+INSERT INTO tasks (burger_name, devoured, toppoings, price) VALUES ('Assorted Mushroom Burger', false, 2, 5.99);
+INSERT INTO tasks (burger_name, devoured, toppoings, price) VALUES ('Black Bean Burger', false, 2, 5.99);
+INSERT INTO tasks (burger_name, devoured, toppoings, price) VALUES ('Hawaiian Burger', false, 2, 5.99);
+INSERT INTO tasks (burger_name, devoured, toppoings, price) VALUES ('Double Trouble Burger', false, 2, 5.99);
+INSERT INTO tasks (burger_name, devoured, toppoings, price) VALUES ('Pink Paste Burger', false, 2, 5.99);
